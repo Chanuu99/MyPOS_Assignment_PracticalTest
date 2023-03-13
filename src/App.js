02f6785c715f23a9cbdb95b1c9a74c324d0e4ef8
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2023-03-13 13:55:12 +0530

    initialized frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AppRoutes from "./routes/app-routes";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className = "App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
+
